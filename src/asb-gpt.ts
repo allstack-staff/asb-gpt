fc2a47afae9a629a7f0b9e894aaf43e8029108d5
igor <igorf0660@gmail.com>
2023-08-10 13:00:51 -0300

    feat: modificado arquivo de exportação

--- src/asb-gpt.ts
+++ src/asb-gpt.ts
@@ -1,3 +1,8 @@
-export * from './actions/log';
-export * from './actions/gpt';
-export * from './others/types';
+import Axios from "./others/axiosRequest";
+export default Axios 
+
+
+export * from "./actions/log";
+export * from "./actions/gpt";
+export * from "./others/types";
+
