cb842cfa09425987d052539a6ab371348f13b9f1
igor <igorf0660@gmail.com>
2023-08-10 12:48:06 -0300

    feat: Classe gpt finalizada, aguardando testes

--- src/actions/gpt.ts
+++ src/actions/gpt.ts
@@ -1,77 +1,82 @@
-import axios from "axios";
 import { ConfigGPT, Resp, historyMessages } from "../asb-gpt";
+import Axios from "../asb-gpt";
 
-export class GPT {
+export class GPT extends Axios {
+  private messages: historyMessages;
+  apiKey: string;
+  history: boolean;
+  model: string;
+  max_tokens: number;
+  length: number;
+  defaultContentMessage: { role: string; content: string };
 
-    private messages: historyMessages;
+  constructor({
+    apiKey,
+    history = true,
+    model = "gpt-3.5-turbo",
+    max_tokens = 500,
+  }: ConfigGPT) {
+    super();
+    this.apiKey = apiKey;
+    this.history = history;
+    this.model = model;
+    this.max_tokens = max_tokens;
+    this.length = 20;
+    this.defaultContentMessage = {
+      role: "system",
+      content: "You are a helpful assistant.",
+    };
+    this.messages = [];
+  }
 
-    private models = ['text-davinci-0011', "davinci", 'text-search-curie-query-001', "gpt-3.5-turbo"];
+  public setHistory({ role, content }, length: number) {
+    if (role === undefined || content === undefined)
+      throw new Error("role or content is undefined");
+    this.messages.push({ role, content });
+    this.length = length === undefined ? this.length : length;
+  }
 
-    apiKey: string;;
-    history: boolean;
-    model: string;;
-    max_tokens: number;;
+  public async requestChat(body: string): Promise<{ data: Resp }> {
+    if (body === undefined) throw new Error("body is undefined");
 
-    constructor({ apiKey, history = true, model = "gpt-3.5-turbo", max_tokens = 500 }: ConfigGPT) {
+    const content = body.trim();
 
-        this.apiKey = apiKey
-        this.history = history
-        this.model = model
-        this.max_tokens = max_tokens
+    if (this.history) this.messages.push({ role: "user", content });
 
-        this.messages = [
-            {
-                role: "system",
-                content: "You are a helpful assistant who never responds to anything related to violence or sexual content. Keep the answers as short as possible."
-            }
-        ];
-
-    }
-
-    public async requestChat(body: string): Promise<{ data: Resp }> {
-        if (body === undefined) throw new Error("body is undefined")
-
-        const content = body.trim();
-
-        if (this.history) this.messages.push({ role: "user", content });
-
-        try {
-            const options = {
-                method: 'POST',
-                url: 'https://api.openai.com/v1/chat/completions',
-                headers: {
-                    'Content-Type': 'application/json',
-                    Authorization: `Bearer ${this.apiKey}`
-                },
-                data: {
-                    model: this.model,
-                    messages: this.history ? this.messages : [],
-                    max_tokens: this.max_tokens,
-                }
-            };
-
-            const res = await axios.request(options);
-
-            const resp: Resp = {
-                response: res.data.choices[0].message || res.data.error,
-                usage: res.data.usage || null,
-            };
-
-            if (resp.response) {
-                if (this.messages.length >= 20) this.messages.splice(1, 20);
-
-                this.messages.push({
-                    role: "user",
-                    content: resp.response as string
-                });
-            }
-
-            return { data: resp };
-        } catch (error) {
-            console.error(error);
-            throw new Error("Erro ao executar a solicitação do OpenAI");
+    try {
+      const res = await this.axiosRequest(
+        "POST",
+        "https://api.openai.com/v1/chat/completions",
+        {
+          model: this.model,
+          messages: this.history
+            ? this.messages
+            : () => {
+                this.messages = [];
+                this.messages.push(this.defaultContentMessage);
+              },
+          max_tokens: this.max_tokens,
         }
+      );
+
+      const resp: Resp = {
+        response: res.data.choices[0].message || res.data.error,
+        usage: res.data.usage || null,
+      };
+
+      if (resp.response) {
+        if (this.messages.length >= this.length) this.messages.splice(1, (this.length - 5))
+
+        this.messages.push({
+          role: "user",
+          content: resp.response as string,
+        });
+      }
+
+      return { data: resp };
+    } catch (err) {
+      console.error(err);
+      throw new Error(`Erro ao executar a solicitação: ${err}`);
     }
-
-
+  }
 }
