b0c2e95e908495e3a3f57042830a5d73bb01f53d
Igor Ferreira <igorf0660@gmail.com>
2023-12-13 06:30:43 -0300

    asb-openai fora do controlador

---
+++ src/asb-openai.ts
@@ -0,0 +1,9 @@
+import openaiRequest from "./others/openaiRequest";
+export default openaiRequest;
+
+export * from "./actions/log";
+export * from "./actions/gpt";
+export * from "./actions/dalle";
+export * from "./others/types";
+export * from "./others/util";
+export * from "./exceptions/WriteStreamError";
